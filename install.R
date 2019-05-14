7b77c80613fb89c8fb8266fd1c8b18911fbe6a7c
Andrew Heiss <andrew@andrewheiss.com>
2019-05-14 16:50:42 -0600

    Install R Markdown stuff

--- install.R
+++ install.R
@@ -1,6 +1,12 @@
 install.packages("tidyverse")
+
+# R Markdown stuff
 install.packages("rmarkdown")
 install.packages("knitr")
+install.packages("caTools")
+install.packages("bitops")
+
+# Project-specific stuff
 install.packages("ggridges")
 install.packages("here")
 install.packages("huxtable")
